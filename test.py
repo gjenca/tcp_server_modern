17cfa2f5d5676996aeeba0c09f00a69b08300c2a
Gejza Jenča <gejza.jenca@stuba.sk>
2024-03-17 08:01:51 +0100

    argparse

--- test.py
+++ test.py
@@ -2,16 +2,22 @@
 import socket
 import time
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("host",default='localhost',help='host',nargs='?')
+parser.add_argument("port",default=9999,type=int,help='port',nargs='?')
+args=parser.parse_args()
 
 socks = []
 for i in range(200):
     print('socket no.',i)
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     print('about to connect',i)
-    sock.connect(("127.0.0.1",9999))
+    sock.connect((args.host,args.port))
     print('connected',i)
     socks.append((sock,sock.makefile('rw',encoding='utf-8')))
-    time.sleep(0.005)
+    #time.sleep(0.005)
 
 for i,(sock,f) in enumerate(socks):
     f.write(f'ping {i}\n')

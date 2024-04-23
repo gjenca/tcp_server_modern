653408498d2e6321802daeab38038bdf2e27e8c9
Gejza Jenča <gejza.jenca@stuba.sk>
2024-04-23 12:49:45 +0200

    ChatGPT doplnil diakritiku

--- server_lock.py
+++ server_lock.py
@@ -3,58 +3,57 @@ import socket
 import multiprocessing
 import logging
 
-# nastavime uroven protokolovania (logging)
+# nastavíme úroveň protokolovania (logging)
 logging.basicConfig(level=logging.INFO)
 
 
-# toto je funkcia, ktora bezi v osobitnom procese
-def handle_client(client_socket,addr,l):
+# toto je funkcia, ktorá beží v osobitnom procese
+def handle_client(client_socket, addr, l):
 
-    logging.info(f'handle_client {addr} start')
-    sf=client_socket.makefile(mode='rw',encoding='utf-8')
+    logging.info(f'handle_client {addr} začiatok')
+    sf = client_socket.makefile(mode='rw', encoding='utf-8')
     while True:
-        # precitame jeden riadok
-        line=sf.readline()
+        # prečítame jeden riadok
+        line = sf.readline()
         if not line:
             # klient zavrel spojenie
             break
-        line=line.rstrip()
+        line = line.rstrip()
         # zamkneme;
-        # ak je l v stave "zamknute", l.acquire() blokuje
-        # az do odomknutia
+        # ak je l v stave "zamknuté", l.acquire() blokuje
+        # až do odomknutia
         l.acquire()
-        # ZACIATOK KRITICKEJ SEKCIE
-        # precitame, co je v tomto okamihu v subore data.txt
-        with open('data.txt','r') as f:
-            previous=f.readline().strip()
-        # zapiseme don to, co nam poslal klient cez socket
-        with open('data.txt','w') as f:
+        # ZAČIATOK KRITICKEJ SEKCIE
+        # prečítame, čo je v tomto okamihu v súbore data.txt
+        with open('data.txt', 'r') as f:
+            previous = f.readline().strip()
+        # zapíšeme doň to, čo nám poslal klient cez socket
+        with open('data.txt', 'w') as f:
             f.write(f'{line}\n')
         # KONIEC KRITICKEJ SEKCIE
         # odomkneme
         l.release()
-        # to, co bolo v subore predtym posleme klientovi cez socket s prefixom BUM:
+        # to, čo bolo v súbore predtým, pošleme klientovi cez socket s prefixom BUM:
         sf.write(f'BUM:{previous}\n')
         sf.flush()
-    logging.info(f'handle_client {addr} end')
-    # ked tato funkcia skonci, proces skoncil
-    # (v skutocnosti zostane bezat, Python ho znovupouzije transparentne)
+    logging.info(f'handle_client {addr} koniec')
+    # keď táto funkcia skončí, proces skončil
+    # (v skutočnosti zostane bežať, Python ho znovu použije transparentne)
 
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM) # TCP socket
-s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1) # aby sme mohli ihned restartovat server
-s.bind(('',9999))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP socket
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # aby sme mohli ihneď reštartovať server
+s.bind(('', 9999))
 s.listen(5)
-# vytvorime zdielany Lock
-lock=multiprocessing.Lock()
-# Vytvorime zdielany zdroj: subor data.txt
-# Vsetky procesy, ktore vytvorime budu don citat a zapisovat
-with open('data.txt','w') as f:
+# vytvoríme zdieľaný Lock
+lock = multiprocessing.Lock()
+# Vytvoríme zdieľaný zdroj: súbor data.txt
+# Všetky procesy, ktoré vytvoríme, budú doň čítať a zapisovať
+with open('data.txt', 'w') as f:
     f.write('INIT\n')
 
 while True:
-    cs,addr=s.accept() # prijmeme spojenie, vytvorime proces
-    process=multiprocessing.Process(target=handle_client,args=(cs,addr,lock))
-    process.daemon=True # tento proces pobezi "na pozadi"
+    cs, addr = s.accept()  # prijmeme spojenie, vytvoríme proces
+    process = multiprocessing.Process(target=handle_client, args=(cs, addr, lock))
+    process.daemon = True  # tento proces pobeží "na pozadí"
     process.start()
-
